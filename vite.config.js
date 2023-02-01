d5fce7efb92e21bacc7f46c0b9e17f706334cf1c
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-02-01 09:25:38 +0000

    chore: vite config type support

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   base: '/osser-frontend/',
   plugins: [react()],
